b6b6d759b5bbcf54ed824be34e11812a51884f84
samajohnson <110183021+samajohnson@users.noreply.github.com>
2023-10-29 15:03:35 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BookLover',
+    version='0.1',
+    description='Hw9',
+    url='https://github.com/samajohnson/Hw9',
+    author='Sam Johnson',
+    author_email='nnx3ue@virginia.edu',
+    license='MIT',
+    packages=['BookLover']
+)
